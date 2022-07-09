2b5d247ea2b51ab04a40139d36178aa03830c110
Ramkhi <p.ramkhi.srinivas@gmail.com>
2022-07-09 18:31:39 +0530

    a

---
+++ four.java
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+int a,b,sum;
+printf("Enter first number:");
+scanf("%d", &a);
+printf("Enter second number:");
+scanf("%d", &b);
+sum=a+b;
+printf("Sum of two numbers:");
+scanf("%d", &sum);
+}
+return0;
